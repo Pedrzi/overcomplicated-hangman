9784d8bf916dfba69924299d434b67ad275d3dde
Pedrzi <pedroh9@proton.me>
2026-01-30 16:13:56 +0000

    Add gastar_pontos method to Jogador class for point management

--- Backend/jogador.py
+++ Backend/jogador.py
@@ -7,5 +7,11 @@ class Jogador:
     nome: str
     pontos: int
     palavras_acertadas: list = field(default_factory=list)
-    
+
+
+    def gastar_pontos(self, pontos_gastos: int) -> None:
+        if pontos_gastos > self.pontos:
+            raise ValueError("Pontos insuficientes")
+        self.pontos -= pontos_gastos
+
         
