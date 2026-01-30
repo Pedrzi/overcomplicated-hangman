fe5b41fab5b30d103ac3d245cf34bc5e868d6229
Pedrzi <pedroh9@proton.me>
2026-01-30 16:13:48 +0000

    Refactor Jogador class to use dataclass for cleaner code and remove unnecessary properties

--- Backend/jogador.py
+++ Backend/jogador.py
@@ -1,37 +1,11 @@
+
+from dataclasses import dataclass, field
+
+
+@dataclass
 class Jogador:
-
-    def __init__(self, nome, pontos, lista):
-        self._nome: str = nome
-        self._pontos: int = pontos
-        self._lista = lista
-
-    @property
-    def nome(self) -> str:
-        return self.__nome 
+    nome: str
+    pontos: int
+    palavras_acertadas: list = field(default_factory=list)
     
-    @nome.setter
-    def nome(self, nome):
-        if not nome:
-            raise ValueError("Missing name")
-        elif nome.isalnum() :
-            raise ValueError("Nome não pode conter caracteres especias")
-        self._nome = nome 
-    
-    @property
-    def pontos(self) -> int :
-        return self._pontos
-    
-    @pontos.setter
-    def pontos(self, pontos):
-        if pontos <0 :
-            pontos = 0
-        self._pontos = pontos
-
-    @property
-    def lista(self):
-        return self._lista
-    
-    @lista.setter
-    def lista(self,lista):
-        self._lista
         
