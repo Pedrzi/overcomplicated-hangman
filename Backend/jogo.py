c2ae12d43f1b6007505b89937131de39d64bc28e
Pedrzi <pedroh9@proton.me>
2026-01-30 15:02:22 +0000

    Added type annotations and fixed function calls

--- Backend/jogo.py
+++ Backend/jogo.py
@@ -1,35 +1,37 @@
+"""
+Assumindo já temos uma palavra 
 
-## Assumindo já temos uma palavra 
-
-#histórico = letras que já foram tentadas 
-#tentativas = número de tentativas que ainda pode entrar 
-#número de letras que faltam?? 
-##pontos?
-#número das posições onde a letra certa está na palavra? 
+histórico = letras que já foram tentadas 
+tentativas = número de tentativas que ainda pode entrar 
+número de letras que faltam?? 
+pontos?
+número das posições onde a letra certa está na palavra? 
+"""
 
 class Forca: 
     def __init__ (self, palavra, tamanho, tentativas, historico):
-        self._palavra = palavra
-        self._tamanho = tamanho
+        self._palavra: str = palavra
+        self._tamanho: int = tamanho
         self._tentativas: int = tentativas
-        self._historico = historico
+        self._historico: dict[str, str] = historico
 
 
     def jogada(self, palavra, tamanho, tentativas, historico):
         l = (input("Digite uma letra")).capitalize
         if l in historico:
             print("Tente outra letra")
-            jogada(self, palavra, tamanho, tentativas, historico)
-        elif letracerta(palavra, l):
+        elif self.letracerta(palavra, l):
             historico.append(l)
         else:
-            case (tentativas-1 == 0) : print ("You Lose!")
-            _ : jogada(self, palavra, tamanho, tentaivas -1, historico.append(l))
+            if (tentativas-1 == 0):
+                print("Você perdeu")
+            else:
+                self.jogada(palavra, tamanho, tentativas -1, historico + [l])
     
 
-def letracerta(palavra, letra):
-    for x in palavra:
-        x == letra
+    def letracerta(palavra, letra):
+        for x in palavra:
+            x == letra
 
     
 
